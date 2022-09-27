c4e01ba8bcf5aa5f23e6d81e7aa68ab4c93d180a
benpretorius <54069207+benpretorius@users.noreply.github.com>
2022-09-27 16:36:20 +0200

    Scaffolded Books model. Added seed data function.

---
+++ MVCLibrary/Models/SeedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+      public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryContext(
+                serviceProvider.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                context.RemoveRange(context.Books);
+                context.SaveChanges();
+                if (context.Books.Any())
+                {
+                    return;
+                }
+                context.Books.AddRange(
+                    new Book { Title = "Code Chronicles", CallNumber = "AXD 2029" },
+                    new Book { Title = "Perfume", CallNumber = "AKQ 2229 " }
+                    );
+                context.SaveChanges();
+            }
+           
+
+        }
+    }
+
+}
+
+
+
